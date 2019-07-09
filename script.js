f0903db989d8ef34f893218cddb4f75d17d3d76e
Cody <Mason.Cody.M@gmail.com>
2019-07-08 20:12:35 -0400

    Completes an Etch-a-Sketch with step-wise shading.

--- script.js
+++ script.js
@@ -1,30 +1,47 @@
-const container = document.querySelector('grid');
+const container = document.querySelector('#grid');
+container.addEventListener('mouseover', darken);
+// Hack a workaround to make grid border ignore event listener 
+// intended for its eventually generated child elements.
+container.style.opacity = '1';
+
+const reset = document.querySelector('#reset');
+reset.addEventListener('click', resetGrid);
 
 // Prompt and perform simple validation of received input.
 function getSize() {
-    let gridSize = 1;
-    while (gridSize < 2) {
-        gridSize = prompt('Input a grid length', '2');
+    let gridSize = 0;
+    while (gridSize < 1) {
+        gridSize = parseInt(prompt('Input a grid length', '2'));
     }
     return gridSize;
 }
 
 // Create a clean drawing grid.
 function createGrid(size) {
-    for (let row = 0; row < size; ++row) {
-        for (let col = 0; col < size; ++col) {
-            // let cell = document.createElement('???');
-            container.appendChild(cell);
+    for (let row = 1; row < size + 1; ++row) {
+        for (let col = 1; col < size + 1; ++col) {
+            let cell = document.createElement('span');
+            cell.style.backgroundColor = 'black';
+            cell.style.opacity = '0';
+            // cell.addEventListener('mouseover', darken);
+            cell.style.gridColumn = `${col} / span 1`;
+            cell.style.gridRow = `${row} / span 1`;
+            container.appendChild(cell); 
         }
-        // Anything special to move down a grid row here?
     }
 }
 
-// Clean the drawing grid.
-function resetGrid() {
-    gridSize = getSize();
-    createGrid(size)
+// Darken a grid cell.
+function darken(e) {
+    e.target.style.opacity = Number(e.target.style.opacity) + 0.1;
 }
 
-const reset = document.querySelector('#button');
-reset.addEventListener('click', resetGrid);
+// Reset the drawing grid.
+function resetGrid() {
+    // Clear the current grid of all elements.
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+    // Create a new grid.
+    createGrid(getSize());
+}
